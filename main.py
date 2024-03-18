e1f931b89570001618f70e99506d5f9750a190f5
Ishant <ishanttomar26@gmail.com>
2024-03-18 20:39:11 +0530

    Some few code testing and added more converters

--- main.py
+++ main.py
@@ -1,15 +1,57 @@
 import math
-while True:
-    try:
-        weight = (float(input("Enter your weight (kgs): ")))
-        height = (float(input("Enter your height (m): ")))
-        break
-    except ValueError:
-        print("Please enter numbers")
 
+weight = 1
+height = 1
+def inpvalues():
+    global weight, height
+    while True:
+        try:
+            weight = (float(input("Enter your weight (kgs): ")))
+            height = (float(input("Enter your height (m): ")))
+            break
+        except ValueError:
+            print("Please enter numbers.")
+    return weight, height
+
+pounds = 1
+def pound():
+    global pounds
+    while True:
+        try:
+            pounds = (float(input("Pounds : ")))
+            break
+        except ValueError:
+            print("Please enter numbers.")
+    return pounds
+
+centimeters = 1
+def centimeter():
+    global centimeters
+    while True:
+        try:
+            centimeters = (float(input("Centimeters: ")))
+            break
+        except ValueError:
+            print("Please enter numbers.")
+    return centimeters
+
+foot = 1
+def foots():
+    global foot
+    while True:
+        try:
+            foot = (float(input("Foot: ")))
+            break
+        except ValueError:
+            print("Please enter numbers.")
+    return foot
+
+inpvalues()
 h = height * height
 bmi = float(weight / h)
-text = ""
+cm_m = centimeters/100
+foot_m = foot * 0.3048
+converted_weight = pounds * 0.45359237
 
 if bmi < 18.5:
     text = "You're Underweight."
@@ -24,3 +66,5 @@ elif bmi >= 18.5 and bmi <= 24.9:
 
 print(f"{bmi:.1f}")
 print(text)
+
+# print(f"{converted_weight:.1f}")
